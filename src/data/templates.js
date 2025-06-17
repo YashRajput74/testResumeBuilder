c58bc08b536206beabf1fda6a35eb157bbaad2e7
kritika <kritikabhardwaj2513@gmail.com>
2025-06-17 23:29:04 +0530

    addcssintemplate

--- src/data/templates.js
+++ src/data/templates.js
@@ -21,6 +21,9 @@ const template1 = {
 };
 
 
+
+
+
 const template2 = {
  name: "Classic Gray",
   layout: {
@@ -41,3 +44,95 @@ const template2 = {
     }
   }
 }
+export const templates=[template1,template2];
+
+// export const template3 = {
+//     name: "Modern Blue",
+//     fontFamily: "Lato",
+//     fontSize: "14px",
+//     colorScheme: {
+//       primary: "#3498db",
+//       background: "#ffffff",
+//       text: "#2c3e50"
+//     },
+//     layout: {
+//       templateRows: "auto 1fr",
+//       templateColumns: "1fr 1fr",
+//       areas: [
+//         {
+//           name: "header",
+//           rowStart: 1,
+//           colStart: 1,
+//           rowEnd: 2,
+//           colEnd: 3,
+//           sections: ["personalInfo"]
+//         },
+//         {
+//           name: "leftColumn",
+//           rowStart: 2,
+//           colStart: 1,
+//           rowEnd: 3,
+//           colEnd: 2,
+//           sections: ["education", "workExperience"]
+//         },
+//         {
+//           name: "rightColumn",
+//           rowStart: 2,
+//           colStart: 2,
+//           rowEnd: 3,
+//           colEnd: 3,
+//           sections: ["skills", "projects"]
+//         }
+//       ]
+//     }
+// };
+
+
+
+
+
+// template1.js
+// const template1 = {
+//     name: "Modern Blue",
+//     layout: {
+//         grid: {
+//             templateRows: 'auto 1fr',
+//             templateColumns: '1fr 1fr',
+//             areas: [
+//                 {
+//                     name: 'header',
+//                     colStart: 1,
+//                     colEnd: 3,
+//                     rowStart: 1,
+//                     rowEnd: 2,
+//                     sections: ["personalInfo"]
+//                 },
+//                 {
+//                     name: 'leftColumn',
+//                     colStart: 1,
+//                     colEnd: 2,
+//                     rowStart: 2,
+//                     rowEnd: 3,
+//                     sections: ["education", "workExperience"]
+//                 },
+//                 {
+//                     name: 'rightColumn',
+//                     colStart: 2,
+//                     colEnd: 3,
+//                     rowStart: 2,
+//                     rowEnd: 3,
+//                     sections: ["skills", "projects"]
+//                 },
+//             ]
+//         },
+//         fontFamily: "Lato",
+//         fontSize: "16px",
+//         colorScheme: {
+//             primary: "#3498DB",
+//             background: "#FFFFFF",
+//             text: "#333333"
+//         }
+//     }
+// };
+
+//  export const templates=[template1];
